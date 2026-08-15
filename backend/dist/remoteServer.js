bd7f2c33cf5b19a1e0fafcda1e35f6f8d7611c0d
imparassharma <parasangiras121@gmail.com>
2026-08-15 14:45:36 +0530

    changes

--- backend/dist/remoteServer.js
+++ backend/dist/remoteServer.js
@@ -2,6 +2,9 @@ import { McpServer } from "@modelcontextprotocol/server";
 import { StreamableHTTPTransport } from "@hono/mcp";
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 const remoteServer = new McpServer({
     name: "remoteServer",
     version: "1.0.0",
@@ -11,12 +14,16 @@ remoteServer.registerResource("remoteResource", "https://paras-portfolio-ai.web.
     description: "My portfolio blogs data",
     mimeType: 'text/markdown'
 }, async (uri) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const blogPath = path.join(__dirname, "../src/blogs.md");
+    const blogs = await readFile(blogPath, "utf-8");
     return {
         contents: [
             {
                 uri: uri.href,
                 mimeType: 'text/plain',
-                text: 'Hi I am Paras'
+                text: blogs
             }
         ]
     };
